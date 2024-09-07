2fba2e72b3761d4dc51686be4d5b5cf0ebe5dfbc
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-07 20:16:42 +0300

    finishes lab

---
+++ app/migrations/versions/29bacbfbfb6b_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 29bacbfbfb6b
+Revises: 
+Create Date: 2024-09-07 19:58:30.050609
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '29bacbfbfb6b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
